d28d71a5cfc60c3a86f4fb725db86e596dbe2e6c
Joe <66278762+BarkinSpider@users.noreply.github.com>
2021-01-16 16:01:04 -0800

    Update RenogyWanderer.py

--- RenogyWanderer.py
+++ RenogyWanderer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
-# v0.4a 30/5/20 - Joe Elliott
-
-# This is Alpha Code - writes data to folder /ramdisk in a format to be read by node_exporter, Prometheus/Grafana.
-# Modify the serial interface below to match your system setup.
+# v0.4e 3/1/21 - Joe Elliott
 
 # If you encounter errors like - AttributeError: module 'serial' has no attribute 'Serial'
 # pip3 uninstall pyserial
@@ -18,8 +15,16 @@ import sys, os, io
 import time
 
 debug = False
-# Enter your serial interface below.
-renogy = minimalmodbus.Instrument('/dev/usb1', 1) 
+sleepTime = 10
+devName = '/dev/ttyUSB0'
+
+if (len(sys.argv) > 1):
+        if (sys.argv[1] == "-d"):
+                debug=True
+                sleepTime = 2
+                print("sys.argv[0]: Debug: enabled")
+
+renogy = minimalmodbus.Instrument(devName, 1)
 renogy.serial.baudrate = 9600
 renogy.serial.bytesize = 8
 renogy.serial.parity   = serial.PARITY_NONE
@@ -48,7 +53,7 @@ CHARGING_STATE = {
 if (debug): print(minimalmodbus._get_diagnostic_string())
 
 if (debug):
-        # Print the details of the Wanderer here, also include the config needed to talk to the unit.
+        # Print the details of the power meter here, also include the config needed to talk to the unit.
         print('Details of the serial connection:')
         print("Renogy:", renogy)
 
@@ -58,28 +63,32 @@ def readRenogy(fileObj):
                         register = renogy.read_register(0x00A)
                         maxV = register >> 8
                         maxC = register & 0x00ff
-                        print("Max sys voltage:", float(maxV), "v")
-                        print("Max sys amps:", float(maxC), "a")
+                        if (debug): print("Max sys voltage:", float(maxV), "v")
+                        if (debug): print("Max sys amps:", float(maxC), "a")
 
                         register = renogy.read_register(0x00B)
                         maxD = register >> 8
                         prodType = register & 0x00ff
-                        print("max discharge:", float(maxD), "a")
-                        print("sys type:", prodType, "00=controller 1=inverter")
+                        if (debug): print("max discharge:", float(maxD), "a")
+                        if (debug): print("sys type:", prodType, "00=controller 1=inverter")
 
                 register = renogy.read_register(0x100)
-                print("Battery SOC:", float(register), "%")
+                if (debug): print("Battery SOC:", float(register), "%")
+                valName  = "mode=\"SOC\""
+                valName  = "{" + valName + "}"
+                dataStr  = f"Renogy{valName} {float(register)}"
+                print(dataStr, file=fileObj)
 
                 batVolts = renogy.read_register(0x101)
                 batVolts = batVolts/10
-                if (True): print("Battery Voltage:", float(batVolts), "v")
+                if (debug): print("Battery Voltage:", float(batVolts), "v")
                 valName  = "mode=\"batVolts\""
                 valName  = "{" + valName + "}"
                 dataStr  = f"Renogy{valName} {batVolts}"
                 print(dataStr, file=fileObj)
 
                 register = renogy.read_register(0x102)
-                print("Charging Amps:", float(register/100), "a")
+                if (debug): print("Charging Amps:", float(register/100), "a")
 
                 if (False):     # for chargers with remote temp sensors
                         register = renogy.read_register(0x103)
@@ -94,44 +103,72 @@ def readRenogy(fileObj):
                 temp_value = controller_temp_bits & 0x0ff
                 sign = controller_temp_bits >> 7
                 devTemp = -(temp_value - 128) if sign == 1 else temp_value
-                print("controller temp:", float(devTemp), "C")
+                if (debug): print("controller temp:", float(devTemp), "C")
+                valName  = "mode=\"sccTemp\""
+                valName  = "{" + valName + "}"
+                dataStr  = f"Renogy{valName} {float(devTemp)}"
+                print(dataStr, file=fileObj)
 
                 loadWatts = renogy.read_register(0x106)
-                if (True): print("Load watts:", loadWatts, "w")
+                if (debug): print("Load watts:", loadWatts, "w")
                 valName  = "mode=\"loadWatts\""
                 valName  = "{" + valName + "}"
                 dataStr  = f"Renogy{valName} {loadWatts}"
                 print(dataStr, file=fileObj)
 
                 register = renogy.read_register(0x107)
-                print("PV volts:", float(register/10), "v")
+                if (debug): print("PV volts:", float(register/10), "v")
+                valName  = "mode=\"pvVolts\""
+                valName  = "{" + valName + "}"
+                dataStr  = f"Renogy{valName} {float(register/10)}"
+                print(dataStr, file=fileObj)
 
                 register = renogy.read_register(0x108)
-                print("PV amps:", float(register/100), "a")
+                if (debug): print("PV amps:", float(register/100), "a")
+                valName  = "mode=\"pvAmps\""
+                valName  = "{" + valName + "}"
+                dataStr  = f"Renogy{valName} {float(register/100)}"
+                print(dataStr, file=fileObj)
 
                 pvWatts = renogy.read_register(0x109)
-                if (True): print("PV watts:", pvWatts, "w")
+                if (debug): print("PV watts:", pvWatts, "w")
                 valName  = "mode=\"pvWatts\""
                 valName  = "{" + valName + "}"
                 dataStr  = f"Renogy{valName} {pvWatts}"
                 print(dataStr, file=fileObj)
 
                 register = renogy.read_register(0x10B)
-                print("bat max volts:", float(register)/10, "v")
+                if (debug): print("bat max volts:", float(register)/10, "v")
+                valName  = "mode=\"maxBatV\""
+                valName  = "{" + valName + "}"
+                dataStr  = f"Renogy{valName} {float(register/10)}"
+                print(dataStr, file=fileObj)
 
                 register = renogy.read_register(0x10C)
-                print("bat min volts:", float(register)/10, "v")
+                if (debug): print("bat min volts:", float(register)/10, "v")
+                valName  = "mode=\"minBatV\""
+                valName  = "{" + valName + "}"
+                dataStr  = f"Renogy{valName} {float(register/10)}"
+                print(dataStr, file=fileObj)
 
                 register = renogy.read_register(0x10F)
-                print("todays charge power:", float(register)/1, "w")
+                if (debug): print("todays charge power:", float(register)/1, "w")
+                valName  = "mode=\"todayChgPwr\""
+                valName  = "{" + valName + "}"
+                dataStr  = f"Renogy{valName} {float(register/1)}"
+                print(dataStr, file=fileObj)
 
                 register = renogy.read_register(0x110)
-                print("todays discharge power:", float(register)/1, "w")
+                if (debug): print("todays discharge power:", float(register)/1, "w")
+                valName  = "mode=\"todayDischgPwr\""
+                valName  = "{" + valName + "}"
+                dataStr  = f"Renogy{valName} {float(register/1)}"
+                print(dataStr, file=fileObj)
 
                 register = renogy.read_register(0x120)
                 chargeStateNum = register & 0x00ff
                 chargeStateStr = CHARGING_STATE.get(chargeStateNum)
-                print("Charge state:", chargeStateNum, chargeStateStr)
+                if (debug): print("Charge state:", chargeStateNum, chargeStateStr)
                 valName  = "mode=\"chargeState\""
                 valName  = "{" + valName + ", myStr=\"" + chargeStateStr + "\"}"
                 dataStr  = f"Renogy{valName} {chargeStateNum}"
@@ -139,63 +176,85 @@ def readRenogy(fileObj):
 
 
                 register = renogy.read_register(0xE004)
-                print("Bat Type:", BATTERY_TYPE.get(register), "")
+                batTypeStr = BATTERY_TYPE.get(register)
+                if (debug): print("Bat Type:", batTypeStr)
+                valName  = "mode=\"batType\""
+                valName  = "{" + valName + ", myStr=\"" + batTypeStr + "\"}"
+                dataStr  = f"Renogy{valName} {register}"
+                print(dataStr, file=fileObj)
 
-                return # Code below was for testing on other versions
 
                 register = renogy.read_register(0x113)
-                print("todays gen power:", float(register)/10000, "kw/h")
+                if (debug): print("todays gen power:", float(register)/10, "w/h")
+                valName  = "mode=\"todayGenPwr\""
+                valName  = "{" + valName + "}"
+                dataStr  = f"Renogy{valName} {float(register/10)}"
+                print(dataStr, file=fileObj)
 
                 register = renogy.read_register(0x114)
-                print("todays consumed power:", float(register)/10000, "kw/h")
+                if (debug): print("todays consumed power:", float(register)/10, "w/h")
+                valName  = "mode=\"todayConsumPwr\""
+                valName  = "{" + valName + "}"
+                dataStr  = f"Renogy{valName} {float(register/10)}"
+                print(dataStr, file=fileObj)
 
                 register = renogy.read_register(0x115)
-                print("up days:", register, "days")
+                if (debug): print("up days:", register, "days")
+                valName  = "mode=\"upDays\""
+                valName  = "{" + valName + "}"
+                dataStr  = f"Renogy{valName} {register}"
+                print(dataStr, file=fileObj)
 
                 register = renogy.read_register(0x117)
-                print("battery full cnt:", register, "#")
+                if (debug): print("battery full cnt:", register, "#")
+                valName  = "mode=\"batFullCnt\""
+                valName  = "{" + valName + "}"
+                dataStr  = f"Renogy{valName} {register}"
+                print(dataStr, file=fileObj)
+
+                return
 
                 register = renogy.read_register(0xE003)
                 sysV = register >> 8
                 recV = register & 0x00ff
-                print("Sys bat voltage:", sysV, "v FF(255)==Auto")
-                print("Recon bat voltage:", recV, "v")
+                if (debug): print("Sys bat voltage:", sysV, "v FF(255)==Auto")
+                if (debug): print("Recon bat voltage:", recV, "v")
 
                 register = renogy.read_register(0xE002)
-                print("Bat capacity:", register, "ah")
+                if (debug): print("Bat capacity:", register, "ah")
 
                 register = renogy.read_register(0xE005)
-                print("over voltage:", float(register/10), "v")
+                if (debug): print("over voltage:", float(register/10), "v")
 
                 register = renogy.read_register(0xE006)
-                print("charge voltage:", float(register/10), "v")
+                if (debug): print("charge voltage:", float(register/10), "v")
 
                 register = renogy.read_register(0xE007)
-                print("equalize voltage:", float(register/10), "v")
+                if (debug): print("equalize voltage:", float(register/10), "v")
 
                 register = renogy.read_register(0xE008)
-                print("boost voltage:", float(register/10), "v")
+                if (debug): print("boost voltage:", float(register/10), "v")
 
                 register = renogy.read_register(0xE009)
-                print("float voltage:", float(register/10), "v")
+                if (debug): print("float voltage:", float(register/10), "v")
 
                 register = renogy.read_register(0xE00A)
-                print("boost recovery voltage:", float(register/10), "v")
+                if (debug): print("boost recovery voltage:", float(register/10), "v")
 
                 register = renogy.read_register(0xE00B)
-                print("over discharge recovery voltage:", float(register/10), "v")
+                if (debug): print("over discharge recovery voltage:", float(register/10), "v")
 
                 register = renogy.read_register(0xE00C)
-                print("under voltage warn:", float(register/10), "v")
+                if (debug): print("under voltage warn:", float(register/10), "v")
 
                 register = renogy.read_register(0xE00D)
-                print("over discharge:", float(register/10), "v")
+                if (debug): print("over discharge:", float(register/10), "v")
 
                 register = renogy.read_register(0xE00E)
-                print("discharge warn voltage:", float(register/10), "v")
+                if (debug): print("discharge warn voltage:", float(register/10), "v")
 
                 register = renogy.read_register(0xE012)
-                print("Boost time:", register, "mins")
+                if (debug): print("Boost time:", register, "mins")
 
         except IOError:
                 print("Failed to read from instrument")
@@ -208,11 +267,12 @@ while True:
 
         # write data here
 
-        if (True): print("\nReading Renogy Wanderer data...")
+        if (debug): print("\nReading Renogy Wanderer data...")
         readRenogy(file_object)
 
         file_object.flush()
         file_object.close()
         outLine = os.system('/bin/mv /ramdisk/Renogy.prom.tmp /ramdisk/Renogy.prom')
 
-        time.sleep(10)
+        time.sleep(sleepTime)
+        
